5b8b84a4c2d35be29f9490331af67751f8ba8f5a
Diptanil Chaudhuri <diptanil.chaudhuri@gmail.com>
2022-10-22 14:00:00 -0500

    Added test with random action

--- src/app.py
+++ src/app.py
@@ -3,5 +3,24 @@ from .env.loadenv import Environment
 class Application:
     def __init__(self, env: str, soln: str):
 
-        print(f"Loading environment : {env}")
-        self.environment = Environment(env)
+        self._env = Environment(env).env
+
+        self.test_random_act()
+
+        
+    # Checks everything is working by running the environment with random action
+    def test_random_act(self):
+
+        self._env.action_space.seed(42)
+
+        observation, info = self._env.reset(seed=42)
+
+        for _ in range(1000):
+            observation, reward, terminated, truncated, info = self._env.step(self._env.action_space.sample())
+
+            if terminated or truncated:
+                observation, info = self._env.reset()
+
+        self._env.close()
+
+
--- src/env/loadenv.py
+++ src/env/loadenv.py
@@ -1,4 +1,6 @@
 import gym
+from gym.utils.env_checker import check_env
+# from gym.utils.play import play
 
 ENVMAP : dict[str, str] = {
     'berzerk' : 'ALE/Berzerk-v5',
@@ -12,8 +14,24 @@ class Environment:
             env_str = ENVMAP[env]
         except:
             raise ValueError('Environment not found')
+        
         #Loading the environment
-        self._env = gym.make(env_str)
+        print(f"Loading environment : {env}")
+
+        self._env = gym.make(env_str,
+        obs_type='rgb',                 # ram / rgb / greyscale
+        frameskip=4,                    # frame skip
+        difficulty=None,
+        repeat_action_probability=0.25, # sticky action probability
+        full_action_space=False,        # use all actions 
+        render_mode = 'human'           # None / human / rgb_array
+        )
+
+        # Doing environment check
+        # print("Checking Environment")
+        # check_env(self._env.unwrapped)
+
+
     
     @property
     def env(self):
